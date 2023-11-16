c17181f99e6a9e780d237ded468b7017df2a60af
Bruka25 <bkteshome862@gmail.com>
2023-11-16 14:19:23 +0300

    opocode pint error

---
+++ pint_err.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ *empty_stack_err -  Outputs Pint error messages for empty stacks
+ *
+ *@line_number: Line number in Monty bytecodes file where error occurred
+ *
+ * Return: EXIT_FAILURE
+ */
+int pint_err(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	return (EXIT_FAILURE);
+}
